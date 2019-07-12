16d5eb777cfa4dc309b2870f911983874a473739
ritu <ritu.prasad@lntinfotech.com>
2019-07-12 15:02:49 -0700

    This is the first commit of day one

---
+++ OnlineExamProjectStart/src/main/java/com/example/demo/OnlineExamProjectStartApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineExamProjectStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineExamProjectStartApplication.class, args);
+		System.out.println("BAllu ki maki");
+	}
+
+}
